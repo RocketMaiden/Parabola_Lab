0cc36a03071ae1934fbd4be54a1826072e9907a8
RocketMaiden <elizabeth.ruchko@gmail.com>
2023-07-02 10:45:37 +0300

    parabola constructor created

--- Parabola_Lab/Program.cs
+++ Parabola_Lab/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.WriteLine("Enter your X1 coordinate");
+﻿using Parabola_Lab;
+
+Console.WriteLine("Enter your X1 coordinate");
 float x1;
 float.TryParse(Console.ReadLine(), out x1);
 
@@ -10,5 +12,18 @@ Console.WriteLine("Enter your height");
 float h;
 float.TryParse(Console.ReadLine(), out h);
 
+Console.WriteLine("Enter your step for drawing a parabola");
+float step;
+float.TryParse(Console.ReadLine(), out step);
 
+Parabola parabola = new Parabola(x1, x2, h);
+
+float x = x1;
+
+while (x <= x2)
+{
+    Console.WriteLine($"{x}, {parabola.Draw(x)}");
+
+    x += step;
+}
 
