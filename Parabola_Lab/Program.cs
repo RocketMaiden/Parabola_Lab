44547cff2a96ac460028887f20d14d3336fa9e6d
RocketMaiden <elizabeth.ruchko@gmail.com>
2023-07-02 10:22:19 +0300

    user input

--- Parabola_Lab/Program.cs
+++ Parabola_Lab/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Enter your X1 coordinate");
+float x1;
+float.TryParse(Console.ReadLine(), out x1);
+
+Console.WriteLine("Enter your X2 coordinate");
+float x2;
+float.TryParse(Console.ReadLine(), out x2);
+
+Console.WriteLine("Enter your height");
+float h;
+float.TryParse(Console.ReadLine(), out h);
+
+
+
